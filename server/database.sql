3ef8f934fdd2620cdb81250f7e39b8ca2d2dcae9
RamneekKaur983 <ramneek983@gmail.com>
2021-05-23 17:11:07 +0530

    Movieticket

---
+++ server/database.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE MovieTicket;
+
+CREATE TABLE Seat(
+    seat_id SERIAL PRIMARY KEY  , 
+    movie_id INT ,
+    isBooked BOOLEAN NOT NULL 
+
+);
+
+CREATE TABLE Movie(
+    movie_id SERIAL PRIMARY KEY  , 
+    name VARCHAR(255),
+    genre VARCHAR(255) , 
+    date DATE  
+
+);
+
+CREATE TABLE Image(
+
+    image_id SERIAL PRIMARY KEY , 
+    movie_id INT , 
+    imgname VARCHAR(255) , 
+    img BYTEA 
+)
